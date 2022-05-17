7103b06615795b6f98b1b1936cbf4df8211668eb
jevin.kim <ja201820@gmail.com>
2022-05-17 16:00:10 +0900

    code update

--- __init__.py
+++ __init__.py
@@ -19,29 +19,48 @@ def index():
     return render_template('index.html')
 
 @app.route('/test_get')
-def nst_get():
+def test_get():
     return render_template('test_get.html')
 
 @app.route('/test_post', methods=['GET', 'POST'])
-def nst_post():
+def test_post():
     if request.method == 'POST':
         # Reference Image
         refer_img = request.form['refer_img']
-        refer_img_path = 'static/images/test_get/' + str(refer_img)
+        refer_img_path = 'images/test_get/' + str(refer_img)
 
         # User Image (target image)
         user_img = request.files['user_img']
         user_img.save('./aws-flask/static/images/' + str(user_img.filename))
-        user_img_path = 'static/images/' + str(user_img.filename)
+        user_img_path = 'images/' + str(user_img.filename)
 
         # Neural Style Transfer
         # transfer_img = neural_style_transfer.main(refer_img_path, user_img_path)
-        # transfer_img_path = '.static/images/'+str(transfer_img.split('/')[-1])
+        # transfer_img_path = 'images/'+str(transfer_img.split('/')[-1])
+        transfer_img_path = 'images/test_result_/test_reference1.png'
 
     return render_template('test_post.html', refer_img=refer_img_path,
-                           user_img=user_img_path)
+                           user_img=user_img_path, transfer_img=transfer_img_path)
 
 
+@app.route('/ocr_get')
+def ocr_get():
+    return render_template('ocr_get.html')
+
+@app.route('/ocr_post', methods=['GET', 'POST'])
+def ocr_post():
+    if request.method == 'POST':
+
+        # User Image (target image)
+        ocr_img = request.files['ocr_img']
+        ocr_img.save('./aws-flask/static/images/' + str(ocr_img.filename))
+        ocr_img_path = 'images/' + str(ocr_img.filename)
+
+        # Optical Character Recognition
+        # tr_img = neural_style_transfer.main(refer_img_path, user_img_path)
+        # transfer_img_path = '.static/images/'+str(transfer_img.split('/')[-1])
+
+    return render_template('test_post.html', ocr_img=ocr_img_path)
 
 
 
